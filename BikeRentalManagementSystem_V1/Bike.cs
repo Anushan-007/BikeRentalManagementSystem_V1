7fd125726427e5d714b0f6fb4998bb97358530c1
Maheswaran A. <UT01317@unicomtic.com>
2024-10-11 09:50:14 +0530

    2nd ques

---
+++ BikeRentalManagementSystem_V1/Bike.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentalManagementSystem_V1
+{
+    internal class Bike
+    {
+        private string bikeId { get; set; }
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private decimal RentalPrice { get; set; }
+
+
+    }
+}
