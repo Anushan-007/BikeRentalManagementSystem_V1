1054ffa0e84a5b4957aefeffaed9ae4c716da3c8
Maheswaran A. <UT01317@unicomtic.com>
2024-10-11 10:02:58 +0530

    3rd Question

--- BikeRentalManagementSystem_V1/Bike.cs
+++ BikeRentalManagementSystem_V1/Bike.cs
@@ -8,10 +8,20 @@ namespace BikeRentalManagementSystem_V1
 {
     internal class Bike
     {
+        public Bike(string bikeId, string Brand, string Model, decimal RentalPrice)
+        {
+           
+        }
         private string bikeId { get; set; }
         private string Brand { get; set; }
         private string Model { get; set; }
         private decimal RentalPrice { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Bike ID: {bikeId}, Brand: {Brand}, Model: {Model}, Rental Price: {RentalPrice}\r\n";
+        }
 
 
     }
--- BikeRentalManagementSystem_V1/Program.cs
+++ BikeRentalManagementSystem_V1/Program.cs
@@ -10,7 +10,9 @@ namespace BikeRentalManagementSystem_V1
     {
         static void Main(string[] args)
         {
-
+            var Bike1 = new Bike("BIKE_001", "YAMAHA", "mt-15", 10.00M);
+            Console.WriteLine(Bike1.ToString());
+            Console.ReadLine();
         }
     }
 }
